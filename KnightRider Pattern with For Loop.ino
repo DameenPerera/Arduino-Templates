fd751caf28ebcca03f2a4dbb8462544afb3c320e
DameenPerera <dameencc95@gmail.com>
2024-07-27 12:20:21 +0530

    KnightRider Pattern with For Loops

---
+++ KnightRider Pattern with For Loop.ino
@@ -0,0 +1,22 @@
+void setup() {
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  for (int x = 1; x <= 7; x++){
+    digitalWrite(x, HIGH);
+    delay(200);
+    digitalWrite(x, LOW);
+  }
+  for (int y = 6; y > 1; y--){
+    digitalWrite(y, HIGH);
+    delay(200);
+    digitalWrite(y, LOW);
+  }
+}
